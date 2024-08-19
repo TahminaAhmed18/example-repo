c90fb4129326c6a959bdaf6effdaaa89a4ca3dba
Tahmina Ahmed <tahminajoyee18@gmail.com>
2024-08-19 16:09:25 +0930

    Fix state transition logic in ENFA

--- src/ENFA.java
+++ src/ENFA.java
@@ -1,22 +1,28 @@
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Stack;
 
 public class ENFA {
     private State startState;
     private Set<State> acceptStates;
 
+    // Constructor
     public ENFA(State startState, Set<State> acceptStates) {
         this.startState = startState;
         this.acceptStates = acceptStates;
     }
 
+    // Getter for start state
     public State getStartState() {
         return startState;
     }
 
+    // Getter for accept states
     public Set<State> getAcceptStates() {
         return acceptStates;
     }
 
+    // Method to create an ENFA for a single character
     public static ENFA character(char c) {
         State start = new State(0);
         State accept = new State(1);
@@ -24,39 +30,7 @@ public class ENFA {
         return new ENFA(start, Set.of(accept));
     }
 
-    public static ENFA union(ENFA a, ENFA b) {
-        State start = new State(0);
-        start.addEpsilonTransition(a.getStartState());
-        start.addEpsilonTransition(b.getStartState());
-        Set<State> acceptStates = new HashSet<>();
-        acceptStates.addAll(a.getAcceptStates());
-        acceptStates.addAll(b.getAcceptStates());
-        return new ENFA(start, acceptStates);
-    }
-
-    public static ENFA concat(ENFA a, ENFA b) {
-        for (State accept : a.getAcceptStates()) {
-            accept.addEpsilonTransition(b.getStartState());
-        }
-        return new ENFA(a.getStartState(), b.getAcceptStates());
-    }
-
-    public static ENFA star(ENFA a) {
-        State start = new State(0);
-        State accept = new State(1);
-        start.addEpsilonTransition(a.getStartState());
-        start.addEpsilonTransition(accept);
-        for (State acceptState : a.getAcceptStates()) {
-            acceptState.addEpsilonTransition(a.getStartState());
-            acceptState.addEpsilonTransition(accept);
-        }
-        return new ENFA(start, Set.of(accept));
-    }
-
-    public static ENFA plus(ENFA a) {
-        return concat(a, star(a));
-    }
-
+    // Method to match an input string against the ENFA
     public boolean matches(String input) {
         Set<State> currentStates = epsilonClosure(Set.of(startState));
         for (char c : input.toCharArray()) {
@@ -65,7 +39,11 @@ public class ENFA {
                 nextStates.addAll(state.getTransitions(c));
             }
             currentStates = epsilonClosure(nextStates);
+            if (currentStates.isEmpty()) {
+                return false; // Early exit if no valid transitions exist
+            }
         }
+        // Check if any of the current states are accepting states
         for (State state : currentStates) {
             if (acceptStates.contains(state)) {
                 return true;
@@ -74,6 +52,7 @@ public class ENFA {
         return false;
     }
 
+    // Method to compute the epsilon closure of a set of states
     private Set<State> epsilonClosure(Set<State> states) {
         Set<State> closure = new HashSet<>(states);
         Stack<State> stack = new Stack<>();
