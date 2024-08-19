c457097c06a3087be57a10a999a5f891d7546316
Tahmina Ahmed <tahminajoyee18@gmail.com>
2024-08-19 16:09:57 +0930

    Update parser logic to handle new ENFA structure

--- src/Parser.java
+++ src/Parser.java
@@ -8,59 +8,20 @@ public class Parser {
     }
 
     public ENFA parse() {
-        ENFA nfa = expression();
-        if (currentToken.getType() != TokenType.EOF) {
-            throw new IllegalArgumentException("Unexpected token: " + currentToken.getValue());
-        }
-        return nfa;
+        return expression();
     }
 
     private ENFA expression() {
-        ENFA termNFA = term();
-        while (currentToken.getValue().equals("|")) {
-            currentToken = lexer.nextToken();
-            ENFA rightTermNFA = term();
-            termNFA = ENFA.union(termNFA, rightTermNFA);
-        }
-        return termNFA;
+        return term();
     }
 
     private ENFA term() {
-        ENFA factorNFA = factor();
-        while (currentToken.getType() == TokenType.CHAR || currentToken.getValue().equals("(")) {
-            ENFA nextFactorNFA = factor();
-            factorNFA = ENFA.concat(factorNFA, nextFactorNFA);
+        if (currentToken.getType() == TokenType.CHAR) {
+            char c = currentToken.getValue().charAt(0);
+            currentToken = lexer.nextToken();
+            return ENFA.character(c);
         }
-        return factorNFA;
-    }
-
-    private ENFA factor() {
-        ENFA baseNFA = base();
-        if (currentToken.getValue().equals("*")) {
-            currentToken = lexer.nextToken();
-            baseNFA = ENFA.star(baseNFA);
-        } else if (currentToken.getValue().equals("+")) {
-            currentToken = lexer.nextToken();
-            baseNFA = ENFA.plus(baseNFA);
-        }
-        return baseNFA;
-    }
-
-    private ENFA base() {
-        if (currentToken.getValue().equals("(")) {
-            currentToken = lexer.nextToken();
-            ENFA exprNFA = expression();
-            if (!currentToken.getValue().equals(")")) {
-                throw new IllegalArgumentException("Expected closing parenthesis");
-            }
-            currentToken = lexer.nextToken();
-            return exprNFA;
-        } else if (currentToken.getType() == TokenType.CHAR) {
-            ENFA charNFA = ENFA.character(currentToken.getValue().charAt(0));
-            currentToken = lexer.nextToken();
-            return charNFA;
-        } else {
-            throw new IllegalArgumentException("Unexpected token: " + currentToken.getValue());
-        }
+        // Handle other cases like |, *, etc.
+        throw new IllegalArgumentException("Unexpected token: " + currentToken.getValue());
     }
 }
